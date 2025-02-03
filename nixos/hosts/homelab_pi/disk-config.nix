515fa994ca193bbb5c5d370c565e7ee5863586be
Asbjørn Rysgaard Eriksen <mail@bliztle.com>
2025-02-03 23:22:14 +0100

    first attempt at disko

--- nixos/hosts/homelab_pi/disk-config.nix
+++ nixos/hosts/homelab_pi/disk-config.nix
@@ -3,19 +3,14 @@
 {
   disko.devices = {
     disk.disk1 = {
-      device = lib.mkDefault "/dev/sda";
+      device = lib.mkDefault "/dev/mmcblk1";
       type = "disk";
       content = {
         type = "gpt";
         partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
           esp = {
             name = "ESP";
-            size = "500M";
+            size = "30M";
             type = "EF00";
             content = {
               type = "filesystem";
