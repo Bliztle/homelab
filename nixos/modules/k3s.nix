01802fea6868a18d65e1f8c62e9ae60da30b9aea
Asbjørn Rysgaard Eriksen <mail@bliztle.com>
2025-02-12 20:33:15 +0100

    reverted to regular hardware config

--- nixos/modules/k3s.nix
+++ nixos/modules/k3s.nix
@@ -1,8 +1,17 @@
-{ ... }:
+{ meta, ... }:
 
 {
   services.k3s = {
     enable = true;
-    role = "server";
+    role = meta.role;
+    token = "INSERT_TOKEN_VALUE";
+    clusterInit = meta.hostname == "homelab-lenovo";
+    serverAddr = if meta.hostname == "homelab-lenovo" then "" else "homelab-lenovo.home";
+    extraFlags = toString [
+      "--write-kubeconfig-mode \"644\""
+      "--disable servicelb"
+      "--disable traefik"
+      "--disable local-storage"
+    ];
   };
 }
