e5353d72cb370ec9d419e3e17161fe60ab788c12
Asbjørn Rysgaard Eriksen <mail@bliztle.com>
2025-02-22 14:05:38 +0100

    write kubeconfig mode required to start kubes

--- nixos/modules/k3s.nix
+++ nixos/modules/k3s.nix
@@ -7,5 +7,8 @@
     tokenFile = "/var/lib/homelab/k3s/token";
     clusterInit = meta.hostname == "homelab-lenovo";
     serverAddr = if meta.hostname == "homelab-lenovo" then "" else "https://10.0.0.7:6443";
+    extraFlags = toString [
+      "--write-kubeconfig-mode \"0644\""
+    ];
   };
 }
