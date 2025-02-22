7edbad78208f373d9a18146f198ba4cce86dfa83
Asbjørn Rysgaard Eriksen <mail@bliztle.com>
2025-02-22 14:52:51 +0100

    agents do not work with kubeconfig flag

--- nixos/modules/k3s.nix
+++ nixos/modules/k3s.nix
@@ -1,5 +1,7 @@
 { meta, ... }:
-
+let
+  isServer = meta.role == "server";
+in
 {
   services.k3s = {
     enable = true;
@@ -7,8 +9,10 @@
     tokenFile = "/var/lib/homelab/k3s/token";
     clusterInit = meta.hostname == "homelab-lenovo";
     serverAddr = if meta.hostname == "homelab-lenovo" then "" else "https://10.0.0.7:6443";
-    extraFlags = toString [
+    extraFlags = toString ([ 
+
+    ] ++ (if isServer then [
       "--write-kubeconfig-mode \"0644\""
-    ];
+    ] else []));
   };
 }
